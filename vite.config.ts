1727d64d960d862836d72e2867eb718039b38f05
MroZ11 <zhoushu1118@qq.com>
2022-01-07 17:19:45 +0800

    增加gitpage打包环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -10,8 +10,12 @@ import path from 'path'
  * @type {import('vite').UserConfig}
  * @description https://vitejs.dev/config/
  */
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ command, mode }) => {
+
+  const { VITE_BUILD_OUT_DIR, VITE_BASE_URL } = loadEnv(mode, process.cwd())
+  
+  return {
+    plugins: [
       vue(),
       Components({
         resolvers: [ElementPlusResolver()],
@@ -19,20 +23,22 @@ export default defineConfig({
       viteMockServe({
         // default
         mockPath: './src/request/mock',
-      
       }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      'vue': 'vue/dist/vue.esm-bundler.js'
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        'vue': 'vue/dist/vue.esm-bundler.js'
+      }
+    },
+    build: {
+      sourcemap: true,
+      outDir: VITE_BUILD_OUT_DIR ? VITE_BUILD_OUT_DIR : 'dist'
+    },
+    base: VITE_BASE_URL ? VITE_BASE_URL : '/',
+    server: {
+      port: 5200,
+      open: true
     }
-  },
-  build:{
-    sourcemap:true,
-  },
-  server:{
-    port:5200,
-    open:true
   }
 })
